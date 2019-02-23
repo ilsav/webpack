f0a5586cbe9c7392d1011db28640291a0f9fdeff
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 14:49:35

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -2,7 +2,12 @@
 const { path: PROJECT_ROOT } = require('app-root-path');
 const { resolve } = require('path');
 
+// Directories
 exports.PROJECT_ROOT = PROJECT_ROOT;
 exports.SOURCE = resolve(PROJECT_ROOT, './source');
 exports.BUILD = resolve(PROJECT_ROOT, './build');
 exports.STATIC = resolve(PROJECT_ROOT, './static');
+
+// Network
+exports.HOST = 'localhost';
+exports.PORT = 3000;
