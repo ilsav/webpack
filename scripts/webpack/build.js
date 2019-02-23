0a065573fbd18582d8d092b85e6175af2fe23c65
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 13:20:00

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -2,11 +2,10 @@
  * 1. ✓ webpack
  * 2. ✓ webpack config
  * 3. ✓ compiler
- * 4. init
+ * 4. ✓ init
  */
 
 // Core
-
 const webpack = require('webpack');
 const getConfig = require('./config/webpack.config');
 
