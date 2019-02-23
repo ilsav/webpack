ecd97755e4a984882945e208d63df0aa66c7dbb8
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 13:36:06

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -33,4 +33,16 @@ compiler.run((error, stats) => {
 
     console.log(info);
     console.log(chalk.greenBright('✓ Build completed!'));
+
+    if (stats.hasErrors()) {
+        // ошибка во время компиляции (битый импорт, ошибка синтаксиса, etc)
+        console.log(chalk.redBright('→ Error!'));
+        console.error(info);
+    }
+
+    if (stats.hasWarnings()) {
+        // ворнинг во время компиляции
+        console.log(chalk.yellowBright('→ Warning!'));
+        console.warn(info);
+    }
 });
