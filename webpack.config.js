644ac1a000301e147b58e093271b27e3513e0438
ilyasavin <ilya.savin@corp.kismia.com>
2019-02-23 12:11:39 +0200

    PRACT-1: clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 // Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+let pathsToClean = [
+  'dist',
+  'build',
+];
+
+let cleanOptions = {
+  // root: '/full/webpack/root/path',
+}
+
 
 /**
  * object
@@ -16,6 +27,10 @@ module.exports = () => {
             filename: 'bundle.js',
         },
         plugins: [
+            new CleanWebpackPlugin(
+                pathsToClean,
+                cleanOptions,
+            ),
             new HtmlWebpackPlugin({
                 template: './static/template.html',
                 title:    'Учим вебпак! 💪🏼🌟🔫',
