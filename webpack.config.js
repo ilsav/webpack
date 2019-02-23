cd8926efbb17dc0904def69509acef6e985a06d2
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 11:39:50

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode:    'none',
     devtool: false,
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './static/template.html',
+            title:    'Учим вебпак! 💪🏼🌟🔫',
+        }),
+    ],
 };
